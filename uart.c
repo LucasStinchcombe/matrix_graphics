23ec91caa4c129fed5bfdd64182c51de46bc9ff5
onetwo <lucasstinchcombe@gmail.com>
2019-01-14 11:58:40 +0900

    Add uart support

---
+++ uart.c
@@ -0,0 +1,31 @@
+#include <avr/io.h>
+
+#define MYFOSC 16000000UL
+#define BAUD 9600
+#define MYUBRR MYFOSC / 16 / BAUD - 1
+
+void UART_init(unsigned int ubrr)
+{
+    // set baud rate registers
+    UBRR0H = (unsigned char)(ubrr >> 8);
+    UBRR0L = (unsigned char)ubrr;
+
+    // disable double speed
+    UCSR0A &= ~(1 << U2X0);
+
+    // enable transmitter bit
+    UCSR0B = (1 << TXEN0);
+
+    // 8 bit data
+    // asynchronous
+    // no parity bit
+    // one stop bit
+    UCSR0C = (1 << UCSZ00) | (1 << UCSZ01);
+}
+
+void UART_transmit(unsigned char data)
+{
+    while (!(UCSR0A & (1 << UDRE0)))
+        ;
+    UDR0 = data;
+}
---
+++ uart.h
@@ -0,0 +1,8 @@
+#ifndef ONETWO_UART_H
+#define ONETWO_UART_H
+
+#include <avr/io.h>
+
+void UART_init(unsigned int ubrr);
+void UART_transmit(unsigned char data);
+#endif
