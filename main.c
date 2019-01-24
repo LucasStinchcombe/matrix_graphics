6abc5b4ef5a82ab48d336768ba7fc1d36b5d7748
onetwo <lucasstinchcombe@gmail.com>
2019-01-24 15:40:43 +0900

    Display tetrahedron

--- main.c
+++ main.c
@@ -32,21 +32,25 @@ int main()
     // disable shutdown mode
     send_matrix_code(0x0C01);
 
-    camera_t g_camera_t = {.position = {0.5, 0.5, -3.0},
+    camera_t camera = {.position = {0.5, 0.5, -2.25},
                        .orientation = {0.0, 0.0, 0.0},
+                       //.orientation = {0.0, 0.0, 0.0},
                        .focal_length = 2.0};
 
     /*
-    g_camera_t.position.x -= 0.5;
-    g_camera_t.position.y -= 0.5;
-    g_camera_t.position.z -= 0.5;
+    camera.position.x -= 0.5;
+    camera.position.y -= 0.5;
+    camera.position.z -= 0.5;
 
     angle3d_t tb= {0.0, -M_PI_4, 0.0};
-    rotate_basis(&tb, &g_camera_t.position);
+    rotate_basis(&tb, &camera.position);
 
-    g_camera_t.position.x += 0.5;
-    g_camera_t.position.y += 0.5;
-    g_camera_t.position.z += 0.5;
+    INFO_LOG("camera position (%f,%f,%f)\n",
+            camera.position.x, camera.position.y, camera.position.z);
+
+    camera.position.x += 0.5;
+    camera.position.y += 0.5;
+    camera.position.z += 0.5;
     */
 
     vector3d_t cube[12];
@@ -65,18 +69,27 @@ int main()
     cube[11] = (vector3d_t){{1.0, 0.0, 1.0}, {1.0, 0.0, 0.0}};
     */
 
+    /*
     cube[0] = (vector3d_t){{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}};
     cube[1] = (vector3d_t){{0.0, 0.0, 0.0}, {0.0, 1.0, 0.0}};
     cube[2] = (vector3d_t){{0.0, 1.0, 0.0}, {1.0, 1.0, 0.0}};
     cube[3] = (vector3d_t){{1.0, 0.0, 0.0}, {1.0, 1.0, 0.0}};
+    */
 
-    for (int i = 0; i != 4; ++i)
+    cube[0] = (vector3d_t){{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}};
+    cube[1] = (vector3d_t){{0.0, 0.0, 0.0}, {0.5, 0.81649658071, 0.28867513459 }};
+    cube[2] = (vector3d_t){{1.0, 0.0, 0.0}, {0.5, 0.81649658071, 0.28867513459 }};
+    cube[3] = (vector3d_t){{0.5, 0.0, 0.86602540378}, {0.5, 0.81649658071, 0.28867513459 }};
+    cube[4] = (vector3d_t){{0.5, 0.0, 0.86602540378}, {0.0, 0.0, 0.0 }};
+    cube[5] = (vector3d_t){{0.5, 0.0, 0.86602540378}, {1.0, 0.0, 0.0 }};
+
+    for (int i = 0; i != 6; ++i)
     {
-        vector2d_t vec = {perspective(&g_camera_t, cube[i].p1),
-                         perspective(&g_camera_t, cube[i].p2)};
+        vector2d_t vec = {perspective(&camera, cube[i].p1),
+                         perspective(&camera, cube[i].p2)};
         matrix_basis(&vec);
 
-        LOG("projected ((%f,%f), (%f,%f))\n", vec.p1.x, vec.p1.y, vec.p2.x,
+        DEBUG_LOG("projected ((%f,%f), (%f,%f))\n", vec.p1.x, vec.p1.y, vec.p2.x,
             vec.p2.y);
         matrix_draw(vec);
         send_matrix();
