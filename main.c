49e5393ed62de173744842bed8eaecdee4381566
onetwo <lucasstinchcombe@gmail.com>
2019-01-26 12:44:15 +0900

    Add small demo program with joystick

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <util/delay.h>
 
 // TODO cleanup includes for all sources
+#include "adc.h"
 #include "conway.h"
 #include "debug.h"
 #include "graphics.h"
@@ -14,6 +15,7 @@
 int main()
 {
     LOG_INIT();
+    ADC_init();
 
     matrix_init();
 
@@ -32,72 +34,62 @@ int main()
     // disable shutdown mode
     send_matrix_code(0x0C01);
 
-    camera_t camera = {.position = {0.5, 0.5, -2.25},
-                       .orientation = {0.0, 0.0, 0.0},
-                       //.orientation = {0.0, 0.0, 0.0},
-                       .focal_length = 2.0};
+    // MUX to 0
+    vector3d_t object[12];
 
-    /*
-    camera.position.x -= 0.5;
-    camera.position.y -= 0.5;
-    camera.position.z -= 0.5;
+    object[0] = (vector3d_t){{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}};
+    object[1] =
+        (vector3d_t){{0.0, 0.0, 0.0}, {0.5, 0.81649658071, 0.28867513459}};
+    object[2] =
+        (vector3d_t){{1.0, 0.0, 0.0}, {0.5, 0.81649658071, 0.28867513459}};
+    object[3] = (vector3d_t){{0.5, 0.0, 0.86602540378},
+                             {0.5, 0.81649658071, 0.28867513459}};
+    object[4] = (vector3d_t){{0.5, 0.0, 0.86602540378}, {0.0, 0.0, 0.0}};
+    object[5] = (vector3d_t){{0.5, 0.0, 0.86602540378}, {1.0, 0.0, 0.0}};
 
-    angle3d_t tb= {0.0, -M_PI_4, 0.0};
-    rotate_basis(&tb, &camera.position);
-
-    INFO_LOG("camera position (%f,%f,%f)\n",
-            camera.position.x, camera.position.y, camera.position.z);
-
-    camera.position.x += 0.5;
-    camera.position.y += 0.5;
-    camera.position.z += 0.5;
-    */
-
-    vector3d_t cube[12];
-    /*
-    cube[0] = (vector3d_t){{0.0, 0.0, 0.0}, {0.0, 0.0, 1.0}};
-    cube[1] = (vector3d_t){{0.0, 0.0, 0.0}, {0.0, 1.0, 0.0}};
-    cube[2] = (vector3d_t){{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}};
-    cube[3] = (vector3d_t){{0.0, 0.0, 1.0}, {0.0, 1.0, 1.0}};
-    cube[4] = (vector3d_t){{0.0, 0.0, 1.0}, {1.0, 0.0, 1.0}};
-    cube[5] = (vector3d_t){{0.0, 1.0, 0.0}, {0.0, 1.0, 1.0}};
-    cube[6] = (vector3d_t){{1.0, 1.0, 1.0}, {1.0, 1.0, 0.0}};
-    cube[7] = (vector3d_t){{1.0, 1.0, 1.0}, {1.0, 0.0, 1.0}};
-    cube[8] = (vector3d_t){{1.0, 1.0, 1.0}, {0.0, 1.0, 1.0}};
-    cube[9] = (vector3d_t){{1.0, 1.0, 0.0}, {1.0, 0.0, 0.0}};
-    cube[10] = (vector3d_t){{1.0, 1.0, 0.0}, {0.0, 1.0, 0.0}};
-    cube[11] = (vector3d_t){{1.0, 0.0, 1.0}, {1.0, 0.0, 0.0}};
-    */
-
-    /*
-    cube[0] = (vector3d_t){{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}};
-    cube[1] = (vector3d_t){{0.0, 0.0, 0.0}, {0.0, 1.0, 0.0}};
-    cube[2] = (vector3d_t){{0.0, 1.0, 0.0}, {1.0, 1.0, 0.0}};
-    cube[3] = (vector3d_t){{1.0, 0.0, 0.0}, {1.0, 1.0, 0.0}};
-    */
-
-    cube[0] = (vector3d_t){{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}};
-    cube[1] = (vector3d_t){{0.0, 0.0, 0.0}, {0.5, 0.81649658071, 0.28867513459 }};
-    cube[2] = (vector3d_t){{1.0, 0.0, 0.0}, {0.5, 0.81649658071, 0.28867513459 }};
-    cube[3] = (vector3d_t){{0.5, 0.0, 0.86602540378}, {0.5, 0.81649658071, 0.28867513459 }};
-    cube[4] = (vector3d_t){{0.5, 0.0, 0.86602540378}, {0.0, 0.0, 0.0 }};
-    cube[5] = (vector3d_t){{0.5, 0.0, 0.86602540378}, {1.0, 0.0, 0.0 }};
-
-    for (int i = 0; i != 6; ++i)
-    {
-        vector2d_t vec = {perspective(&camera, cube[i].p1),
-                         perspective(&camera, cube[i].p2)};
-        matrix_basis(&vec);
-
-        DEBUG_LOG("projected ((%f,%f), (%f,%f))\n", vec.p1.x, vec.p1.y, vec.p2.x,
-            vec.p2.y);
-        matrix_draw(vec);
-        send_matrix();
-    }
+    float rot_y = 0;
     while (1)
     {
+        matrix_clear();
+        camera_t camera = {.position = {0.5, 0.5, -2.25},
+                           .orientation = {0.0, -rot_y, 0.0},
+                           .focal_length = 2.0};
+
+        camera.position.x -= 0.5;
+        camera.position.y -= 0.28867513459;
+        camera.position.z -= 0.28867513459;
+
+        angle3d_t tb = {0.0, rot_y, 0.0};
+        rotate_basis(&tb, &camera.position);
+
+        INFO_LOG("camera position (%f,%f,%f)\n", camera.position.x,
+                 camera.position.y, camera.position.z);
+
+        camera.position.x += 0.5;
+        camera.position.y += 0.28867513459;
+        camera.position.z += 0.28867513459;
+
+        for (int i = 0; i != 6; ++i)
+        {
+            vector2d_t vec = {perspective(&camera, object[i].p1),
+                              perspective(&camera, object[i].p2)};
+            matrix_basis(&vec);
+
+            DEBUG_LOG("projected ((%f,%f), (%f,%f))\n", vec.p1.x, vec.p1.y,
+                      vec.p2.x, vec.p2.y);
+            matrix_draw(vec);
+        }
+
+        ADCSRA |= (1 << ADSC);
+        while (ADCSRA & (1 << ADSC))
+            ;
+
+        uint8_t val = get_value(0);
+        INFO_LOG("ADC value: %d\n", val);
+        float y = (val / 1.0 - 129.0) / 256 * 0.25;
+        rot_y += y;
         send_matrix();
         // next_iter();
-        _delay_ms(500);
+        //_delay_ms(500);
     }
 }
